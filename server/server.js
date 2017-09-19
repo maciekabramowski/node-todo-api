ae9270452d88133ef5e73033792b40e6b47a7009
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-19 20:42:44 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -13,14 +14,14 @@ app.post('/todos', (req, res)=>{
   var todo = new Todo({
     text: req.body.text
   });
-
+//
   todo.save().then((doc)=>{
     res.send(doc);
   }, (e)=>{
     res.status(400).send(e);
   });
 });
-
+//
 app.get('/todos', (req, res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -28,9 +29,26 @@ app.get('/todos', (req, res)=>{
     res.status(400).send(e);
   });
 });
+//
+app.get('/todos/:id', (req, res)=> {
+  var id = req.params.id;
 
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+//
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
 });
-
+//
 module.exports = {app};
